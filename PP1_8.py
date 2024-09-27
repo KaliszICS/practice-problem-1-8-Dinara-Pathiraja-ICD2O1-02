42ab2b7f750bae521807daeeb4994b6eefeccc2c
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-09-27 18:46:50 +0000

    b

--- PP1_8.py
+++ PP1_8.py
@@ -1,9 +1,13 @@
 
+
 def q1():
-  #Write Assignment code here
+  bool1 = True
+  bool2 = False
+  print (True and False)
+  print (True or False)
 
 def q2():
-  #Write Assignment code here
+  integer1= input("Input an integer: ")
 
 def q3():
   #Write Assignment code here
@@ -18,7 +22,7 @@ def q5():
 #Comment out the followwing code when running tests
 
 q1()
-q2()
-q3()
-q4()
-q5()
+# q2()
+# q3()
+# q4()
+# q5()
