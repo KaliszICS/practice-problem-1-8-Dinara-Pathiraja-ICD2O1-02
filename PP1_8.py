1c3cc737006bf204f67c8f6d27a31c83c7b9f9dd
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-09-28 21:44:28 +0000

    w

--- PP1_8.py
+++ PP1_8.py
@@ -1,27 +1,46 @@
 
 
 def q1():
-  bool1 = True
-  bool2 = False
-  print (True and False)
-  print (True or False)
+  bool1 = (True)
+  bool2 = (False)
+  print (bool1 and bool2)
+  print (bool1 or bool2)
 
 def q2():
-  integer1= input("Input an integer: ")
+  prompt = input('Enter an integer: ')
+  integer1 = int(prompt)
+  bool1 = True 
+  bool1 = (integer1 != 0)
+  bool2 = False 
+  bool2 = (integer1 == 0)
+  print (bool1 or bool2)
 
 def q3():
-  #Write Assignment code here
+  ask = input ("Enter a number: ")
+  number1 = int (ask)
+  bool1 = True 
+  bool1 = (0 < number1 and 10 > number1)
+  print (bool1)
 
 def q4():
-  #Write Assignment code here
+  food = input("Input food: ")
+  drink = input ("Input drink: ")
+  bool1 = False 
+  bool1 = (food == "pizza" and drink == "pop")
+  print(bool1 or not bool1)
+
 
 def q5():
-  #Write Assignment code here
+  ask = input("Enter an integer: ")
+  integer1 = int(ask)
+  bool1 = True 
+  bool1 = (integer1 % 2 == 0)
+  print (bool1 or not bool1)
 
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
-q1()
+# q1()
 # q2()
 # q3()
 # q4()
