c9817bfa0839fb33039e3c651c3a8258f6195532
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-10-15 18:37:10 +0000

    d

--- PP1_8.py
+++ PP1_8.py
@@ -7,35 +7,35 @@ def q1():
   print (bool1 or bool2)
 
 def q2():
-  prompt = input('Enter an integer: ')
+  prompt = input("Enter an integer: ")
   integer1 = int(prompt)
   bool1 = True 
   bool1 = (integer1 != 0)
-  bool2 = False 
-  bool2 = (integer1 == 0)
-  print (bool1 or bool2)
+  print (bool1)
 
 def q3():
   ask = input ("Enter a number: ")
-  number1 = int (ask)
-  bool1 = True 
-  bool1 = (0 < number1 and 10 > number1)
+  number1 = float (ask)
+  bool1 = True
+  bool1 = (0 <= number1 and 10 >= number1)
+  #bool1 = (0 <= number1 <=10)
   print (bool1)
 
 def q4():
   food = input("Input food: ")
-  drink = input ("Input drink: ")
-  bool1 = False 
-  bool1 = (food == "pizza" and drink == "pop")
-  print(bool1 or not bool1)
+  drink = input("Input drink: ")
+  #bool1 = False
+  bool1 =not(food == "pizza" and drink == "pop") 
+  print(bool1)
 
 
 def q5():
   ask = input("Enter an integer: ")
   integer1 = int(ask)
-  bool1 = True 
-  bool1 = (integer1 % 2 == 0)
-  print (bool1 or not bool1)
+  bool2 = True 
+  bool2 = (integer1 % 2 == 0)
+  print (f"The integer {integer1} is {bool2}.")
+
 
 #Do not edit code after this
 #Comment out the followwing code when running tests
